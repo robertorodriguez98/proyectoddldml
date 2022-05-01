4cfeac940057ac3c01e31e563166d498831364b7
Roberto Rodríguez <krowno.iso@gmail.com>
2022-05-01 20:01:36 +0200

    creada la tabla objetos

--- ORACLE/tablas.sql
+++ ORACLE/tablas.sql
@@ -20,3 +20,19 @@ CONSTRAINT CK_Mapa_Bioma CHECK (BIOMA IN ('Selva','Desierto','Montaña'))
 
 INSERT INTO Mapa VALUES('Selva Jurásica',15,'Selva');
 
+CREATE TABLE Objeto(
+nombre VARCHAR2 (20),
+mapa_nombre VARCHAR2 (20),
+monstruo_nombre VARCHAR2 (20),
+valor DECIMAL (8,2),
+rareza DECIMAL(2),
+conseguido DATE,
+CONSTRAINT PK_Objetos PRIMARY KEY (nombre),
+CONSTRAINT FK_Objetos1 FOREIGN KEY (mapa_nombre) REFERENCES Mapa(nombre),
+CONSTRAINT FK_Objetos2 FOREIGN KEY (monstruo_nombre) REFERENCES Monstruo(nombre),
+CONSTRAINT CK_Objetos_Rareza CHECK (rareza >= 1 AND rareza <= 10),
+CONSTRAINT CK_Objetos_Valor CHECK (valor >= 0 AND valor <= 10000),
+CONSTRAINT CK_Objetos_Conseguido CHECK (conseguido > TO_DATE('01/01/2010','DD/MM/YYYY') AND Conseguido <= TO_DATE('01/05/2022','DD/MM/YYYY') )
+);
+
+INSERT INTO Objeto VALUES ('Garra de Fatalis','Selva Jurásica','Fatalis Carmesi',1500,5,TO_DATE('01/01/2015','DD/MM/YYYY'));
