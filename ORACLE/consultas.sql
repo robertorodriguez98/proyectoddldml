ee4bbf9ac764b61970f8a7fab70dd1138478bdce
Roberto Rodríguez <krowno.iso@gmail.com>
2022-05-02 20:02:16 +0200

    creadas alteraciones de tablas

--- ORACLE/consultas.sql
+++ ORACLE/consultas.sql
@@ -1,2 +1,15 @@
 alter table Monstruo
 add peso DECIMAL (8,2);
+
+alter table Monstruo
+modify(
+    peso VARCHAR2(11)
+);
+
+alter table Monstruo
+add constraint CK_Monstruo_Peso CHECK (TO_NUMBER(peso) >= 0);
+
+alter table Monstruo
+disable constraint CK_Monstruo_Peso;
+
+insert into Monstruo values('Fatalis Verde','Dragón Anciano',2723,'-1');
