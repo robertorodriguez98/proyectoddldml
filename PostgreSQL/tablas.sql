2ee55fe9b8ad2747a9eb5c8a1bef2e7f1bcc0895
Roberto Rodríguez <krowno.iso@gmail.com>
2022-05-02 18:21:24 +0200

    creada la tabla Mapa

--- PostgreSQL/tablas.sql
+++ PostgreSQL/tablas.sql
@@ -8,5 +8,15 @@ CONSTRAINT CK_Monstruos_Tipo CHECK (TIPO IN ('Anfibio','Bestia de Colmillos','Ca
 CONSTRAINT CK_Monstruos_Nombre CHECK (nombre ~ '^(\s?[A-Z][a-z]*)+$')
 );
 
+CREATE TABLE Mapa(
+nombre VARCHAR (20) PRIMARY KEY,
+nZonas DECIMAL (2) NOT NULL,
+bioma VARCHAR (20),
+CONSTRAINT CK_Mapa_nZonas CHECK (nZonas >= 1 AND nZonas <= 20),
+CONSTRAINT CK_Mapa_Bioma CHECK (BIOMA IN ('Selva','Desierto','Montaña'))
+);
+
 INSERT INTO Monstruo VALUES ('Fatalis Carmesi','Dragón Anciano',2723);
+
+INSERT INTO Mapa VALUES('Selva Jurásica',15,'Selva');
 
