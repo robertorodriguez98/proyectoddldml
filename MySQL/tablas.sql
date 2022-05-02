de5edf4767b24de6de1563cdc4e952b523998dd1
Roberto Rodríguez Márquez <68345903+robertorodriguez98@users.noreply.github.com>
2022-05-02 02:04:03 +0200

    Añadidos valores

--- MySQL/tablas.sql
+++ MySQL/tablas.sql
@@ -58,12 +58,32 @@ CONSTRAINT CK_Equipo_Rareza CHECK (rareza >= 1 AND rareza <= 10)
 
 
 INSERT INTO Monstruo VALUES ('Fatalis Carmesi','Dragón Anciano',2723);
+INSERT INTO Monstruo VALUES ('Nergigante','Dragón Anciano',3500);
+INSERT INTO Monstruo VALUES ('Teostra','Dragón Anciano',2723);
+INSERT INTO Monstruo VALUES ('Anjanath','Wyvern Bruto',2723);
+INSERT INTO Monstruo VALUES ('Paolumu','Wyvern Volador',2723);
+INSERT INTO Monstruo VALUES ('Vaal Hazak','Dragón Anciano',2723);
 
 INSERT INTO Mapa VALUES('Selva Jurásica',15,'Selva');
+INSERT INTO Mapa VALUES('Yermo de Agujas',7,'Desierto');
+INSERT INTO Mapa VALUES('Altiplanos Coralinos',17,'Selva');
+INSERT INTO Mapa VALUES('Valle Putrefacto',10,'Desierto');
+INSERT INTO Mapa VALUES('Arroyo de Escarcha',12,'Montaña');
 
 INSERT INTO Objeto VALUES ('Garra de Fatalis','Selva Jurásica','Fatalis Carmesi',1500,5,'2015-01-01');
+INSERT INTO Objeto VALUES ('Colmillo de Fatalis','Selva Jurásica','Fatalis Carmesi',2000,7,'2015-01-01');
+INSERT INTO Objeto VALUES ('Garra de Anjanath','Selva Jurásica','Anjanath',1000,3,'2015-01-06');
+INSERT INTO Objeto VALUES ('Núcleo de Teostra','Yermo de Agujas','Teostra',3000,8,'2015-01-01');
+INSERT INTO Objeto VALUES ('Alas de Paolumu','Valle Putrefacto','Paolumu',2500,8,'2017-07-01');
 
 INSERT INTO Mision (nombre,mapa_nombre,monstruo_nombre,rango,descripcion,recompensa) VALUES ('Misión de Fatalis','Selva Jurásica','Fatalis Carmesi',5,'Tienes que Matar a un Fatalis Carmesí',1500);
+INSERT INTO Mision (nombre,mapa_nombre,monstruo_nombre,rango,descripcion,recompensa) VALUES ('Mision de Teostra','Yermo de Agujas','Teostra',6,'Tienes que matar a un Teostra',2000);
+INSERT INTO Mision (nombre,mapa_nombre,monstruo_nombre,rango,descripcion,recompensa) VALUES ('Mision de Anjanath','Selva Jurásica','Anjanath',2,'Tienes que matar a un Anjanath',750);
 
 INSERT INTO Equipo VALUES ('Arma de Fatalis','Garra de Fatalis',5,'Arma','Fuego','Rojo');
+INSERT INTO Equipo VALUES ('Armadura Fatalis','Colmillo de Fatalis',6,'Armadura',NULL,NULL);
+INSERT INTO Equipo VALUES ('Hacha de Anjanath','Garra de Anjanath',3,'Arma','Hielo','Naranja');
+INSERT INTO Equipo VALUES ('Casco de Teostra','Núcleo de Teostra',9,'Armadura',NULL,NULL);
+INSERT INTO Equipo VALUES ('Cornamusa de Paolumu','Alas de Paolumu',2,'Arma','Rayo','Amarillo');
 
+COMMIT;
