a47abe0076f7ca05cdffe2b7e399dc62080f110a
Roberto Rodríguez Márquez <68345903+robertorodriguez98@users.noreply.github.com>
2022-05-03 01:47:08 +0200

    creadas consultas

--- MySQL/consultas.sql
+++ MySQL/consultas.sql
@@ -17,4 +17,19 @@ drop column peso;
 
 -- Consultas
 
+select nombre from Monstruo;
+
+create view valor_objetos as
+select nombre, valor from Objeto;
+
+select nombre
+from Equipo
+where Objeto_nombre in (select nombre
+                        from Objeto
+                        where monstruo_nombre= 'Fatalis Carmesi');
+
+select sum(valor)
+from Objeto o, Mision m
+where o.monstruo_nombre = m.monstruo_nombre
+and m.mapa_nombre = 'Selva Jurásica';
 
